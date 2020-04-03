33135878958f6fc346d032aaad9656fa14618de1
ChengBinJin <sbkim0407@gmail.com>
2020-04-03 16:59:14 +0800

    Assignment.lua & Variables.lu

---
+++ Assignment.lua
@@ -0,0 +1,12 @@
+name = "John"
+print(name)
+
+print(lastName)
+
+x, y = 10, 20
+print(x)
+print(y)
+
+x, y = y, x
+print(x)
+print(y)
---
+++ Variables.lua
@@ -0,0 +1,8 @@
+X = 1
+Y = 2
+print("Before")
+print("X: ", X, "Y: ", Y)  -- Swap! (-- is single line comment)
+
+X, Y = Y, X
+print("After")
+print("X: ", X, "Y: ", Y)
