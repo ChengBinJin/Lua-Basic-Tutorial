c0d186eae73bb07ba2ffd67894f3448eceb627fe
ChengBinJin <sbkim0407@gmail.com>
2020-04-08 15:55:49 +0800

    update GuessNumberDemo.lua

--- src/GuessNumberDemo.lua
+++ src/GuessNumberDemo.lua
@@ -3,5 +3,15 @@ math.randomseed(os.time())
 -- math.random()
 number = math.random(100)
 answer = io.read("*n")
+
+if answer < number then
+	print("Too low")
+elseif answer > number then
+	print("Too high")
+else
+	print("You got it")
+end
+
+
 print("number: ", number)
 print("anser: ", answer)
