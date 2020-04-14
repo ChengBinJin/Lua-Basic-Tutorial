f0a5f7a5dd0ded3f6633f28fc9f9872c6b4ac05b
ChengBinJin <sbkim0407@gmail.com>
2020-04-14 20:02:28 +0800

    revise CalorieCounter.lua

--- src/CalorieCounter.lua
+++ src/CalorieCounter.lua
@@ -15,11 +15,27 @@ function CalorieCounter:new(t)
 	return t
 end
 
-c = CalorieCounter:new{goal = 1500}
-c:add(500)
-print(c.count)
-print(c:didReachGoal())
+HealthMonitor = CalorieCounter:new{steps=0, stepGoal=10000}
 
-c:add(1000)
-print(c.count)
-print(c:didReachGoal())
+function HealthMonitor:step(amount)
+	self.steps = self.steps + amount
+end
+
+function HealthMonitor:didReachGoal()
+	local goalsReached = 0
+	if self.count >= self.goal then
+		print("Calorie goal of " .. self.count .. "rached")
+		goalsReached = goalsReached + 1
+	else
+		print("Calorie goal not reached, yet")
+	end
+
+		if self.steps >= self.stepGoal then
+		print("Step goal of " .. self.stepGoal .. "rached")
+		goalsReached = goalsReached + 1
+	else
+		print("Step goal not reached, yet")
+	end
+
+	return goalsReached >= 2
+end
