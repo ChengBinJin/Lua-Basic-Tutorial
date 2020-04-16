315d114ffd2a716730f6a569f4ed922043b0641a
ChengBinJin <sbkim0407@gmail.com>
2020-04-16 21:21:10 +0800

    add StringLibraryDemo.lua

---
+++ src/StringLibraryDemo.lua
@@ -0,0 +1,11 @@
+text = [[hello world!]]
+print(text)
+
+-- text:find("stringToFind") is the same as string.find(text, "stringToFind")
+b, e = text:find("hello w")
+print(b, e)
+print(string.sub(text, b, e))
+
+b, e = text:find("h.-o")
+print(b, e)
+print(string.sub(text, b, e))
