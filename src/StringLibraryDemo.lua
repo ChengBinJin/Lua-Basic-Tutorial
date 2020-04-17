c8db891867f8d46eb3c436c53ad56c227d8dab25
ChengBinJin <sbkim0407@gmail.com>
2020-04-17 13:50:48 +0800

    add StringLibraryDemo.lua

--- src/StringLibraryDemo.lua
+++ src/StringLibraryDemo.lua
@@ -3,9 +3,15 @@ print(text)
 
 -- text:find("stringToFind") is the same as string.find(text, "stringToFind")
 b, e = text:find("hello w")
-print(b, e)
-print(string.sub(text, b, e))
+--print(b, e)
+--print(string.sub(text, b, e))
 
 b, e = text:find("h.-o")
-print(b, e)
-print(string.sub(text, b, e))
+--print(b, e)
+--print(string.sub(text, b, e))
+
+print(text:match("wor"))
+
+for tag in string.gmatch(text, "l") do
+	print("Found", tag)
+end
