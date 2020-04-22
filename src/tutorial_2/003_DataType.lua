ef9a7bff9ba4748a003d71244134f79f71c4aaa6
ChengBinJin <sbkim0407@gmail.com>
2020-04-22 11:20:32 +0800

    update 003_DataType.lua

--- src/tutorial_2/003_DataType.lua
+++ src/tutorial_2/003_DataType.lua
@@ -9,7 +9,7 @@ print(type(print))
 print(type(type))
 print(type(nil))  -- nil "nil"
 print(type(x))
---]]
+
 
 name = "siki"
 print(name)
@@ -25,3 +25,27 @@ print(tab1.key1)
 tab1 = nil
 print(tab1)
 --print(tab1.key2)
+
+
+--boolean
+print(type(true))
+print(type(false))
+print(type(nil))
+
+if true then
+	print("true")
+end
+
+if nil then
+	print("nil works as true")
+else
+	print("nil works as false")
+end
+--]]
+
+print(type(2))
+print(type(2.2))
+print(type(0.2))
+print(type(2e+3))
+print(type(0.2e-1))
+print(type(7.8345435345e-5))
